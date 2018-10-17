a0c2510ef3eaaf69ec754d5e7cc48b96736ee835
Hyun-Su, Mun <munhyunsu@gmail.com>
2018-10-18 00:04:26 +0900

    saver

--- random_forest3.py
+++ random_forest3.py
@@ -1,11 +1,3 @@
-""" Random Forest.
-Implement Random Forest algorithm with TensorFlow, and apply it to classify 
-handwritten digit images. This example is using the MNIST database of 
-handwritten digits as training samples (http://yann.lecun.com/exdb/mnist/).
-Author: Aymeric Damien
-Project: https://github.com/aymericdamien/TensorFlow-Examples/
-"""
-
 from __future__ import print_function
 
 import tensorflow as tf
@@ -14,13 +6,15 @@ from tensorflow.python.ops import resources
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-# Ignore all GPUs, tf random forest does not benefit from it.
 import os
+import random
+
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 
 def get_files(path, ext='', recursive=False):
     path_list = [path]
+    file_list = list()
 
     while len(path_list) > 0:
         cpath = path_list.pop()
@@ -28,66 +22,88 @@ def get_files(path, ext='', recursive=False):
             for entry in it:
                 if not entry.name.startswith('.') and entry.is_file():
                     if entry.name.endswith(ext):
-                        yield entry.path
+                        file_list.append(entry.path)
                     else:
                         if recursive:
                             path_list.append(entry.path)
+    return file_list
 
 
-# Parameters
-num_steps = 500  # Total steps to train
-batch_size = 1024  # The number of samples per batch
-num_classes = 100  # The 10 digits
-num_features = 8  # Each image is 28x28 pixels
-num_trees = 10
-max_nodes = 1000
+def main():
+    # Parameters
+    num_steps = 10  # Total steps to train
+    batch_size = 1024  # The number of samples per batch
+    num_classes = 100  # The 10 digits
+    num_features = 8  # Each image is 28x28 pixels
+    num_trees = 10
+    max_nodes = 1000
 
-X = tf.placeholder(tf.float32, shape=[None, num_features])
-Y = tf.placeholder(tf.int32, shape=[None])
+    X = tf.placeholder(tf.float32, shape=[None, num_features])
+    Y = tf.placeholder(tf.int32, shape=[None])
 
-# Random Forest Parameters
-hparams = tensor_forest.ForestHParams(num_classes=num_classes,
-                                      num_features=num_features,
-                                      num_trees=num_trees,
-                                      max_nodes=max_nodes).fill()
+    # Random Forest Parameters
+    hparams = tensor_forest.ForestHParams(num_classes=num_classes,
+                                          num_features=num_features,
+                                          num_trees=num_trees,
+                                          max_nodes=max_nodes).fill()
 
-# Build the Random Forest
-forest_graph = tensor_forest.RandomForestGraphs(hparams)
-# Get training graph and loss
-train_op = forest_graph.training_graph(X, Y)
-loss_op = forest_graph.training_loss(X, Y)
+    # Build the Random Forest
+    forest_graph = tensor_forest.RandomForestGraphs(hparams)
+    # Get training graph and loss
+    train_op = forest_graph.training_graph(X, Y)
+    loss_op = forest_graph.training_loss(X, Y)
 
-# Measure the accuracy
-infer_op, _, _ = forest_graph.inference_graph(X)
-correct_prediction = tf.equal(tf.argmax(infer_op, 1), tf.cast(Y, tf.int64))
-accuracy_op = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    # Measure the accuracy
+    infer_op, _, _ = forest_graph.inference_graph(X)
+    correct_prediction = tf.equal(tf.argmax(infer_op, 1), tf.cast(Y, tf.int64))
+    accuracy_op = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-# Initialize the variables (i.e. assign their default value) and forest resources
-init_vars = tf.group(tf.global_variables_initializer(),
-    resources.initialize_resources(resources.shared_resources()))
+    # Initialize the variables (i.e. assign their default value) and forest resources
+    init_vars = tf.group(tf.global_variables_initializer(),
+                         resources.initialize_resources(resources.shared_resources()))
 
-# Start TensorFlow session
-sess = tf.Session()
+    saver = tf.train.Saver()
 
-# Run the initializer
-sess.run(init_vars)
+    # Start TensorFlow session
+    sess = tf.Session()
 
-# Training
-for i in range(1, num_steps + 1):
-    # Prepare Data
+    # Run the initializer
+    sess.run(init_vars)
+
+    if os.path.exists('./model/model.ckpt'):
+        saver.restore(sess, './model/model.ckpt')
+
+    # Training
+    for i in range(1, num_steps + 1):
+        # Prepare Data
+        for file in get_files('output', ext='csv'):
+            data = pd.read_csv(file)
+            input_x = data.iloc[:, 0:-1].values
+            input_y = data.iloc[:, -1].values
+            _, l = sess.run([train_op, loss_op], feed_dict={X: input_x, Y: input_y})
+        # file = random.choice(get_files('output', ext='csv'))
+        # data = pd.read_csv(file)
+        # input_x = data.iloc[:, 0:-1].values
+        # input_y = data.iloc[:, -1].values
+        # if i % 50 == 0 or i == 1:
+        #     acc = sess.run(accuracy_op, feed_dict={X: input_x, Y: input_y})
+        #     print('Step %i, Loss: %f, Acc: %f' % (i, l, acc))
+
+    # Test Model
+    accuracy = 0
+    loop = 0
     for file in get_files('output', ext='csv'):
         data = pd.read_csv(file)
         input_x = data.iloc[:, 0:-1].values
         input_y = data.iloc[:, -1].values
-        _, l = sess.run([train_op, loss_op], feed_dict={X: input_x, Y: input_y})
-    if i % 50 == 0 or i == 1:
-        acc = sess.run(accuracy_op, feed_dict={X: input_x, Y: input_y})
-        print('Step %i, Loss: %f, Acc: %f' % (i, l, acc))
+        x_train, x_test, y_train, y_test = train_test_split(input_x, input_y, test_size=0.25)
+        accuracy = accuracy + sess.run(accuracy_op, feed_dict={X: x_test, Y: y_test})
+        loop = loop + 1
+    print("Test Accuracy:", accuracy/loop)
 
-# Test Model
-for file in get_files('output', ext='csv'):
-    data = pd.read_csv(file)
-    input_x = data.iloc[:, 0:-1].values
-    input_y = data.iloc[:, -1].values
-    x_train, x_test, y_train, y_test = train_test_split(input_x, input_y, test_size=0.25)
-    print("Test Accuracy:", sess.run(accuracy_op, feed_dict={X: x_test, Y: y_test}))
+    os.makedirs('./model', exist_ok=True)
+    print('saved path: ', saver.save(sess, './model/model.ckpt'))
+
+
+if __name__ == '__main__':
+    main()
